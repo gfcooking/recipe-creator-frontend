5ed704dfba5aa5ad645f565728e1e6926d9b9720
Matthew D. Scholefield <matthew331199@gmail.com>
2022-12-18 15:32:42 -0600

    Update deploy and yarn lock

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -eE
+
 if ! sudo echo "Building..."; then
     echo "Needs sudo."
     exit 1
@@ -10,13 +12,12 @@ yarn install
 tmp_trash=/tmp/recipe-creator-previous-deployment
 sudo rm -rf "$tmp_trash"
 mkdir "$tmp_trash"
-if ! yarn build; then
-    exit 1
-fi
 
-sudo mv /var/www/html/* "$tmp_trash"
-sudo cp -Rv build/* /var/www/html
+yarn build
+
+sudo mv /opt/gfcooking/* "$tmp_trash" 2>/dev/null || true
+sudo cp -Rv build/* /opt/gfcooking
 echo
-echo "Deployed to /var/www/html."
+echo "Deployed to /opt/gfcooking."
 echo "Previous deployment moved to $tmp_trash"
 
